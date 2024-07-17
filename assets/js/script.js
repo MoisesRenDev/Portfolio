3ad2fc86b805681bd4a7cb7151605b103366d022
Moisés Rentería Gallegos <mrendev@outlook.com>
2024-07-17 16:00:35 -0600

    Portfolio moises ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,21 @@ $(window).on("scroll", function () {
 });
 
 $(document).ready(function () {
+  //Typed Effect
   new Typed("#type-it", {
-    strings: [".NET Developer", "Freelancer"],
+    strings: ["Web Developer", "Freelancer"],
     typeSpeed: 80,
     loop: true,
+  });
+
+  //One page scroll
+  $.scrollIt({
+    upKey: 38, // key code to navigate to the next section
+    downKey: 40, // key code to navigate to the previous section
+    easing: "linear", // the easing function for animation
+    scrollTime: 600, // how long (in ms) the animation takes
+    activeClass: "active", // class given to the active nav element
+    onPageChange: null, // function(pageIndex) that is called when page is changed
+    topOffset: -70, // offste (in px) for fixed top navigation
   });
 });
